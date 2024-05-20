9cadd26feadd35023be2841462d181cbbe490b7b
WardonNE <1730314864@qq.com>
2024-05-20 13:53:35 +0800

    update

--- server_provider.go
+++ server_provider.go
@@ -4,14 +4,25 @@ import (
 	"reflect"
 
 	"github.com/gopi-frame/contract/container"
+	"github.com/gopi-frame/contract/support"
+	"github.com/gopi-frame/contract/validation"
 )
 
 // ServerProvider validation server provider
-type ServerProvider struct{}
+type ServerProvider struct {
+	support.ServerProvider
+}
+
+// Validator get validator instance
+func (s *ServerProvider) Validator(c container.Container) *Validator {
+	return c.Get("validator").(*Validator)
+}
 
 // Register register
 func (s *ServerProvider) Register(c container.Container) {
-	c.Bind(reflect.TypeFor[Validator]().Name(), func(c container.Container) any {
+	c.Bind("validator", func(c container.Container) any {
 		return NewValidator()
 	})
+	c.Alias("validator", reflect.TypeFor[Validator]().String())
+	c.Alias("validator", reflect.TypeFor[validation.Engine]().String())
 }
